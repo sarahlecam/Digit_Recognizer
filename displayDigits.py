1d8e9fe2ebeb333e1497f8c1d1501a4b86920720
Yunie Mao <ym224@cornell.edu>
2017-09-11 20:28:19 -0400

    add KNN classifier and 3Fold cross validation. add confusion matrix plot.

--- displayDigits.py
+++ displayDigits.py
@@ -8,13 +8,16 @@ Created on Sun Sep 10 12:09:31 2017
 """
 
 from matplotlib import pyplot as plt
-import csv
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix 
 import numpy as np
 import scipy as sp
 
-input_file = "train.csv"
+train_data = "train.csv"
+test_data = "test.csv"
+output_file = "digitRecognizer.csv"
 
-def loadData():
+def loadData(input_file):
 	file = open(input_file, 'r')
 	file.readline()
 	data = np.loadtxt(file, delimiter=",").astype(int)
@@ -43,8 +46,6 @@ def displayEachDigit(data):
 
 def displayDigitCounts(data):
 	labels = []
-	file = open(input_file, 'r')
-	file.readline()
 	for row in range(data.shape[0]):
 		label = float(data[row][0])
 		labels.append(label)
@@ -102,12 +103,92 @@ def plotSampleWithNearestNeighbor(nearestNeighbors):
 		plt.imshow(neighborPixels, cmap=plt.get_cmap('gray'))
 	plt.savefig('nearestNeighbors_plot.png')
 	plt.close()
-				
 
-data = loadData()
+
+def KNNClassifierNaive(trainData, classifier, k):
+	# construct list of indices of training data sorted by distance to classifier
+	dist = []
+	for i in range(trainData.shape[0]):
+		dist.append(np.linalg.norm(trainData[i][1:] - classifier))
+	sortedDistIndices = np.argsort(dist)
+
+	# classify sample based on majority vote of class labels on the nearest neighbour list 
+	classCount = {}
+	for i in range(k):
+		neighborIndex = sortedDistIndices[i]
+		label = trainData[neighborIndex][0]
+		classCount[label] = classCount.get(label, 0) + 1
+	majorityClass = sorted(classCount, key=classCount.get, reverse=True)[0]
+	return majorityClass
+
+def perform3FoldCrossValidation(data, k):
+	actual = []
+	predicted = []
+	kf = KFold(n_splits=3, random_state=None, shuffle=False)
+	for train_index, test_index in kf.split(data):
+		trainData = np.array([data[i] for i in train_index])
+		testData = np.array([data[i] for i in test_index])
+		for row in range(testData.shape[0]):
+			predicted.append(KNNClassifierNaive(trainData, testData[row][1:], 10))
+			actual.append(testData[row][0])
+	cm = confusion_matrix(actual, predicted)
+	accuracy = np.array(cm).trace()/np.array(cm).sum()
+	print (cm)
+	print (accuracy)
+	return cm
+
+def plotConfusionMatrix(cm):
+	cm = np.array(cm)
+	plt.figure()
+	plt.imshow(cm)
+	plt.title('Confusion matrix')
+	labels = list(range(10))
+	tick_marks = np.arange(len(labels))
+	plt.xticks(tick_marks, labels, rotation=45)
+	plt.yticks(tick_marks, labels)
+	for i in range(cm.shape[0]):
+		for j in range(cm.shape[1]):
+			plt.text(j, i, format(cm[i, j], 'd'), horizontalalignment="center", color="black")
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+	plt.savefig('confusion_matrix_3Fold_CV.png')
+
+def runKNNClassifierOnTestData(trainData, testData):
+	file = open(output_file, 'w')
+	# write header
+	file.write('ImageId, Label')
+	for row in range(testData.shape[0]):
+		label = KNNClassifierNaive(trainData, testData[row][:], 10)
+		file.write('\n')
+		file.write(str(row+1) + ',' + str(label))
+	file.close()
+
+# load training data
+data = loadData(train_data)
+
+# load test data
+test_data = loadData(test_data)
+
+# display first occurance of each digit and return indices
 digitSample = displayEachDigit(data)
+
+# create histogram of digit counts
 displayDigitCounts(data)
+
+# find indices of nearest neighbor to list of digit samples
 nearestNeighbors = findNearestNeighbor(data, digitSample)
+
+# plot digit sample with its nearest neighbor
 plotSampleWithNearestNeighbor(nearestNeighbors)
+
+# create histogram of pairwise comparison
 binComparisonHist(data)
 
+# create confusion matrix for 3Fold cross validation on training data
+confusion_matrix = perform3FoldCrossValidation(data, 10)
+
+# plot confusion matrix
+plotConfusionMatrix(confusion_matrix)
+
+# run KNN classifier on test data using training data and write result to file
+runKNNClassifierOnTestData(data, test_data)
